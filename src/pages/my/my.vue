9d64467bf3fea63528a2379d69e55cbafc07ac3e
SuStar <3sustar@gmail.com>
2024-01-05 23:45:46 +0800

    添加了请求和上传拦截器

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import '@/utils/http'
 
 const memberStore = useMemberStore()
+
+// 测试请求
+const getData = () => {
+  uni.request({
+    method: 'GET',
+    url: '/home/banner',
+    header: {},
+  })
+}
 </script>
 
 <template>
@@ -11,6 +21,7 @@ const memberStore = useMemberStore()
       @tap="
         memberStore.setProfile({
           nickname: '黑马先锋',
+          token: '12345',
         })
       "
       size="mini"
@@ -20,6 +31,7 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData">test</button>
   </view>
 </template>
 
