8787cbd738933254bbd86f8cd340a1437b32ea6a
SuStar <3sustar@gmail.com>
2024-01-08 21:33:59 +0800

    自动添加组件类型bug修改

--- src/components/components.d.ts
+++ src/components/components.d.ts
@@ -3,6 +3,6 @@ import ShopSwiper from './ShopSwiper.vue'
 // 给自定义组件提供ts类型声明
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ShopSwiper: typeof XtxSwiper
+    ShopSwiper: typeof ShopSwiper
   }
 }
