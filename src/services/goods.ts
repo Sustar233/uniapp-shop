36a2f998332a36983e0afd59ea526e1c91b5cc37
SuStar <3sustar@gmail.com>
2024-01-15 21:17:23 +0800

    商品详情准备工作

---
+++ src/services/goods.ts
@@ -0,0 +1,12 @@
+// 商品详情
+import { http } from '@/utils/http'
+
+export const getGoodsByIdAPI = (id: string) => {
+  return http({
+    method: 'GET',
+    url: './goods',
+    data: {
+      id,
+    },
+  })
+}
