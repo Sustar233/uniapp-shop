7f9caa38338620ef7361e8caec965a99890c112f
SuStar <3sustar@gmail.com>
2024-01-19 05:57:19 +0800

    地址模块，新建地址

---
+++ src/services/address.ts
@@ -0,0 +1,12 @@
+// 添加收货地址
+
+import type { AddressParams } from '@/types/address'
+import { http } from '@/utils/http'
+
+export const postMemberAddressAPI = (data: AddressParams) => {
+  return http({
+    method: 'POST',
+    url: '/member/address',
+    data,
+  })
+}
