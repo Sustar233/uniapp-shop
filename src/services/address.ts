9f836e230c8d512df76c285689a471ffec1f7e07
SuStar <3sustar@gmail.com>
2024-01-20 01:15:28 +0800

    地址管理模块列表渲染

--- src/services/address.ts
+++ src/services/address.ts
@@ -1,6 +1,6 @@
 // 添加收货地址
 
-import type { AddressParams } from '@/types/address'
+import type { AddressItem, AddressParams } from '@/types/address'
 import { http } from '@/utils/http'
 
 export const postMemberAddressAPI = (data: AddressParams) => {
@@ -8,5 +8,14 @@ export const postMemberAddressAPI = (data: AddressParams) => {
     method: 'POST',
     url: '/member/address',
     data,
+  })
+}
+
+// 获取收货地址列表
+
+export const getMemberAddressAPI = () => {
+  return http<AddressItem[]>({
+    method: 'GET',
+    url: '/member/address',
   })
 }
