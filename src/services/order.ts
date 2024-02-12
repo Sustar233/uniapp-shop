7ee13243f3b3d72ee8350ca2731a28596c4264b1
SuStar <3sustar@gmail.com>
2024-02-12 15:21:10 +0800

    完成填写订单

---
+++ src/services/order.ts
@@ -0,0 +1,32 @@
+import { http } from '@/utils/http'
+import type { OrderCreateParams, OrderPreResult } from '@/types/order'
+
+// 获取预付订单
+export const getMemberOrderPreAPI = () => {
+  return http<OrderPreResult>({
+    method: 'GET',
+    url: '/member/order/pre',
+  })
+}
+
+// 获取立即购买订单
+export const getMemberOrderPreNowAPI = (data: {
+  skuId: string
+  count: string
+  addressId?: string
+}) => {
+  return http<OrderPreResult>({
+    method: 'GET',
+    url: '/member/order/pre/now',
+    data,
+  })
+}
+
+// 提交订单
+export const postMemberOrderAPI = (data: OrderCreateParams) => {
+  return http<{ id: string }>({
+    method: 'POST',
+    url: '/member/order',
+    data,
+  })
+}
